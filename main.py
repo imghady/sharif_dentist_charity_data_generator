623b1c41e4ff6ae5b65c9f37bcff5d0a3070f445
imghady <amirreza8010@gmail.com>
2024-01-16 01:29:13 +0330

    compelete dentist table

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import random
+
 import pandas as pd
 
 
 def random_with_n_digits(n):
-    range_start = 10**(n-1)
-    range_end = (10**n)-1
+    range_start = 10 ** (n - 1)
+    range_end = (10 ** n) - 1
     return random.randint(range_start, range_end)
 
 
@@ -13,7 +14,11 @@ xls = pd.ExcelFile('data_model.xlsx')
 location = pd.read_excel(xls, 'Dim_location')
 date = pd.read_excel(xls, 'Dim_date')
 
-speciality = ['general', 'Endodontist', 'Orthodontist', 'Periodontist', 'Prosthodontist', 'Oral Medicine']
+names1 = pd.read_csv("uk-500.csv", usecols=['first_name', 'last_name'])
+names2 = pd.read_csv("us-500.csv", usecols=['first_name', 'last_name'])
+clinics = pd.read_csv("us-500.csv", usecols=['company_name'])
+
+specialities = ['general', 'Endodontist', 'Orthodontist', 'Periodontist', 'Prosthodontist', 'Oral Medicine']
 
 # define tables dataframes
 dentist = pd.DataFrame(columns=['dentist_id', 'name', 'speciality', 'capacity_in_month', 'location_id', 'clinic_name'])
@@ -25,9 +30,21 @@ for i in range(1, 101):
     dentist_id = str(offset + i)
     contact_number = "09" + str(random_with_n_digits(9))
     age = random.randint(4, 14)
-    dentist.add()
+    first_name = names1['first_name'].loc[names1.index[random.randint(0, len(names1) - 1)]]
+    last_name = names1['last_name'].loc[names1.index[random.randint(0, len(names1) - 1)]]
+    name = first_name + " " + last_name
+    speciality = specialities[random.randint(0, len(specialities) - 1)]
+    capacity = random.randint(1, 10)
+    location_id = location['Location_id'].loc[location.index[random.randint(0, len(location) - 1)]]
+    clinic_name = clinics['company_name'].loc[clinics.index[random.randint(0, len(clinics) - 1)]]
+    temp_df = pd.DataFrame(
+        [{'dentist_id': dentist_id, 'name': name, 'speciality': dentist, 'capacity_in_month': capacity,
+          'location_id': location_id, 'clinic_name': clinic_name}])
+    dentist = pd.concat([dentist, temp_df], ignore_index=True)
+
+print(dentist.head())
 
 # generate final file
-with pd.ExcelWriter('output.xlsx') as writer:
-    location.to_excel(writer, sheet_name='location')
-    date.to_excel(writer, sheet_name='date')
+# with pd.ExcelWriter('output.xlsx') as writer:
+#     location.to_excel(writer, sheet_name='location')
+#     date.to_excel(writer, sheet_name='date')
